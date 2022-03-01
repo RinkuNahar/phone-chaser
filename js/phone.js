f3f5e0f49ad6abeb8216e54bebd151be4fa0a5a2
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-03-01 11:22:45 +0600

    main work done

--- js/phone.js
+++ js/phone.js
@@ -4,13 +4,22 @@ const load= () =>{
     searchArea.value = '';
    fetch(`https://openapi.programming-hero.com/api/phones?search=${mine}`)
    .then(res=>res.json())
-   .then(data=>display(data.data));
+   .then(data=>{
+       if(data.length == 0  || data.data != searchArea.value){
+        display(data.data);   
+       }
+       else{
+           alert('No Phone Found');
+       }
+   });
 
 }
  const display= phones=>{
+    
     const main = document.getElementById('main');
     main.innerText = '';
      for(const phone of phones){
+         
          const myDiv = document.createElement('div');
         myDiv.classList.add('col');
         myDiv.innerHTML=`
@@ -34,17 +43,20 @@ const load= () =>{
   .then(data=>displayDetails(data.data));
  }
 
-const displayDetails = id=>{
+const displayDetails = id =>{
     // console.log(id);
     const div = document.getElementById('details');
     div.innerHTML = '';
     const myDiv = document.createElement('div');
     if(id.releaseDate != ''){
         myDiv.innerHTML =`
-        <img src="${id.image}"></img>
-        <h2>${id.name}<h2>
-        <p>${id.releaseDate}</p>
-       
+        <img src="${id.image}">
+        <h3>Name : ${id.name}</h3>
+        <p>Release Date : ${id.releaseDate}</p>
+        <P> Chip set : ${id.mainFeatures.chipSet}</p>
+        <p> Display Size : ${id.mainFeatures.displaySize}</p>
+        <p> Memory : ${id.mainFeatures.memory}</p>
+        <p> Storage : ${id.mainFeatures.storage}</p>
        `;
        div.appendChild(myDiv);
     }
@@ -52,12 +64,18 @@ const displayDetails = id=>{
         alert('No Release Date Found');
         myDiv.innerHTML =`
         <img src="${id.image}"></img>
-        <h2>${id.name}<h2>
-      
+        <h3>Name : ${id.name}</h3>
+        <P> Chip set : ${id.mainFeatures.chipSet}</p>
+        <p> Display Size : ${id.mainFeatures.displaySize}</p>
+        <p> Memory : ${id.mainFeatures.memory}</p>
+        <p> Storage : ${id.mainFeatures.storage}</p>
+       
        `;
        div.appendChild(myDiv);
-     
-   
     }
  
+}
+const displaySensor = id =>{
+  console.log(id.mainFeatures.sensors);
+
 }
